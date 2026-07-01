d9ffea3dd0952466bfe3a74e95fff3435071174b
Anton Okolelov <okoanton@yandex.ru>
2026-07-01 20:01:10 +0200

    ci: configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://anton-okolelov.github.io",
-  base: "nats-toolbox-plugin",
+  base: "/nats-toolbox-plugin",
   output: "static",
 });
